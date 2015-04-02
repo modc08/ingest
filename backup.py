585beb4c9cda60207a386ae21af13cd58d5cc0ce
Jianfeng Li <li@redboxresearchdata.com.au>
2015-04-02 14:24:51 +1030

    Add function to retrieve a backup using date criterion
    if there is a backkup uploaded within one day of the query
    date, retrieve it

--- backup.py
+++ backup.py
@@ -3,7 +3,7 @@
 # pylint: disable=line-too-long,unused-argument
 
 """A basic tool for managing backups on Hitachi Content Platform (which pretends to be S3)."""
-import argparse, glob, sys, os
+import argparse, glob, sys, os, datetime, tzlocal
 import logging
 
 from oagr import HCP
@@ -25,14 +25,33 @@ class Backups(HCP):
     def get_latest(self, to_dir):
         objects = self.list(self.base)
         objects.sort(key=lambda item: item['mtime'], reverse=True)
-        l = objects[0]
+        self.__retrive__(objects[0], to_dir)
+
+    def get_dated(self, to_dir, q_date):
+        # Use q_date to check if a backup was uploaded within one day of it and retrieve it if found
+        #q_date is in local timezone, has to be in YYYYMMDD format
+        #checking from the latest backwards
+        local = tzlocal.get_localzone()
+        dl = local.localize(datetime.datetime.strptime(q_date, '%Y%m%d'))
+        qmtime = (dl-self.epoch).total_seconds()
+
+        objects = self.list(self.base)
+        objects.sort(key=lambda item: item['mtime'], reverse=True)
+        for o in objects:
+            if abs(o["mtime"] - qmtime) < 86400:
+                self.__retrive__(o, to_dir)
+                return
+        logger.info("No backup created within one day around %s" % q_date)
+
+    def __retrive__(self, kd, to_dir):
         try:
-            k = self.bucket.lookup(l["name"])
-            k.get_contents_to_filename("%s/db_backup_%s.gz" % (to_dir, l["mtime"]))
-            logger.info("Downloaded and saved %s/db_backup_%s.gz" % (to_dir, l["mtime"]))
+            k = self.bucket.lookup(kd["name"])
+            k.get_contents_to_filename("%s/db_backup_%s.gz" % (to_dir, kd["mtime"]))
+            logger.info("Downloaded and saved %s/db_backup_%s.gz" % (to_dir, kd["mtime"]))
         except Exception as e:
-            logger.error("Failed to download %s" % l["name"])
+            logger.error("Failed to download %s" % kd["name"])
             logger.error(e)
+
 
 # check backup folder and upload them if the have not in the store
 def upload_backup(config, remove=False):
@@ -54,14 +73,17 @@ def upload_backup(config, remove=False):
     logger.info("Uploading of backups completely")
 
 # get the latest backup, save into a directory
-def retrieve_backup(config):
+def retrieve_backup(config, dated):
     if 'directory' in config:
         directory = config["directory"]
     else:
         directory = '/tmp'
     hcp = Backups(config)
-    hcp.get_latest(directory)
-    logger.info("Retrieving the latest backup completely")
+    if dated:
+        hcp.get_dated(directory, dated)
+    else:
+        hcp.get_latest(directory)
+    logger.info("Retrieving backup completely")
 
 parser = argparse.ArgumentParser(
     description="Back up whatever found in a folder.",
@@ -69,6 +91,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-c", "--config", help="Config file path (optional). Default is ./config.yaml")
 parser.add_argument("-a", "--action", help="Action to be taken: either backup or retrieve the latest backup. ", required=True, choices=['upload', 'retrieve'])
 parser.add_argument("-r", "--remove", help="Remove experiment directory after successful upload", action="store_true")
+parser.add_argument("-d", "--dated", help="Date when a backup was uploaded. Has to be in YYYYMMDD format")
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -86,5 +109,5 @@ if __name__ == "__main__":
         logger.info("Run backup")
         upload_backup(config, args.remove)
     else:
-        logger.info("Get the latest backup from %s of bucket %s in object store" % (config["base"], config["bucket"]))
-        retrieve_backup(config)
+        logger.info("Get a backup from %s of bucket %s in object store" % (config["base"], config["bucket"]))
+        retrieve_backup(config, args.dated)
